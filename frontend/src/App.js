ad1e88bfa1c21fb6d02a89e4b94486f67c282a76
yuwenhuang <yuwenhuang813@gmail.com>
2018-01-21 10:07:33 +0100

    auto replay video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
 import "typeface-roboto";
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import Helmet from 'react-helmet';
+import ShareButton from './shareButton'
 
 const sources = {
   sintelTrailer: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
@@ -17,12 +18,110 @@ const sources = {
 };
 
 
-class App extends Component {
+class Videoplayer extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
       source: sources['bunnyMovie'],
+    };
+
+    this.play = this.play.bind(this);
+    this.pause = this.pause.bind(this);
+    this.load = this.load.bind(this);
+    this.changeCurrentTime = this.changeCurrentTime.bind(this);
+    this.seek = this.seek.bind(this);
+    this.changeVolume = this.changeVolume.bind(this);
+    this.setMuted = this.setMuted.bind(this);
+  }
+
+  componentDidMount() {
+    // subscribe state change
+    this.refs.player.subscribeToStateChange(this.handleStateChange.bind(this));
+  }
+
+  handleStateChange(state, prevState) {
+    // copy player state to this component's state
+    this.setState({
+      player: state,
+    });
+
+    const { player } = this.refs.player.getState();
+    const currentTime = player.currentTime;
+    const ended = player.ended;
+    if(ended){
+      console.log("currentTime:"+currentTime+", isEnd:"+ended);
+      this.refs.player.load();
+    }
+  }
+
+  play() {
+    this.refs.player.play();
+  }
+
+  pause() {
+    this.refs.player.pause();
+  }
+
+  load() {
+    this.refs.player.load();
+  }
+
+  changeCurrentTime(seconds) {
+    return () => {
+      const { player } = this.refs.player.getState();
+      const currentTime = player.currentTime;
+      this.refs.player.seek(currentTime + seconds);
+    };
+  }
+
+  seek(seconds) {
+    return () => {
+      this.refs.player.seek(seconds);
+    };
+  }
+
+  changeVolume(steps) {
+    return () => {
+      const { player } = this.refs.player.getState();
+      const volume = player.volume;
+      this.refs.player.volume = volume + steps;
+    };
+  }
+
+  setMuted(muted) {
+    return () => {
+      this.refs.player.muted = muted;
+    };
+  }
+
+
+  render() {
+    const sourcePath = this.props.path;
+    return (
+      <Player
+        ref="player"
+        autoPlay={true}
+        fluid={false}
+        startTime={49}
+      >
+        <source src={sourcePath} />
+        <ControlBar autoHide={true}>
+          <ShareButton order={7} />
+        </ControlBar>
+      </Player>
+    )
+  }
+  
+}
+
+
+class App extends Component {
+  
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
       showThumbnails: false,
       showNav: true,
       showPlayButton: false,
@@ -32,33 +131,12 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    // subscribe state change
-    this.refs.player1.subscribeToStateChange(this.handleStateChange.bind(this));
-
-  }
-
-  handleStateChange(state, prevState) {
-    // copy player state to this component's state
-    this.setState({
-      player1: state,
-    });
-  }
-
   handleClick = () => {
     console.log('this is:', this);
   }
   _onImageClick(event) {
     console.debug('clicked on image', event.target, 'at index', this._imageGallery.getCurrentIndex());
   }
-
-/* 
-  onClick= {event => {
-    this.slideToIndex.call(this, index, event);
-    if (this.props.onThumbnailClick) {
-      this.props.onThumbnailClick(event, index);
-    }
-  }} */
 
   render() {
     const images = [
@@ -70,15 +148,16 @@ class App extends Component {
       {
         id: 2,
         original: './images/logo.svg',
-        thumbnail: 'http://lorempixel.com/250/150/nature/1/'
+        thumbnail: './images/logo.svg'
       },
       {
         id: 3,
         original: './images/sap-logo-svg.svg',
-        thumbnail: 'fcbayern.jpg'
+        thumbnail: './images/sap-logo-svg.svg'
       }
     ]
 
+  
     return (
       <div className="App">
         <Helmet bodyAttributes={{style: 'background-color : #000000'}}/>
@@ -91,7 +170,7 @@ class App extends Component {
         </p>
       */}
         <div className="container-fluid">
-             <div className="overlayContainer">
+            <div className="overlayContainer">
                 <div className="exactCenter">
                     <div className="bayernLogo">
                         <img src={require('./FCBayern-Logo-3C-W.png')} alt="FCBayern"/>
@@ -117,27 +196,15 @@ class App extends Component {
                             onClick={this._onImageClick.bind(this)}
                             additionalClass="app-image-gallery"/>
                     </div>
-                    <Player
-                      ref="player1"
-                      autoPlay={true}
-                      fluid={false}
-                    >
-                      <source src={this.state.source} />
-                      <ControlBar autoHide={true} />
-                    </Player>
+                    <Videoplayer id="video_player"
+                      path="http://media.w3.org/2010/05/sintel/trailer.mp4" />
                   </div>
                 </div>
 
                 <div className="col-6 d-flex align-items-center">  
                   <div id="video_box">
-                    <Player
-                      ref="player2"
-                      autoPlay={true}
-                      fluid={false}
-                    >
-                      <source src={this.state.source} />
-                      <ControlBar autoHide={true} />
-                    </Player>
+                    <Videoplayer id="video_player"
+                      path="http://media.w3.org/2010/05/bunny/movie.mp4" />
                   </div>    
                 </div>
         </div>
@@ -152,7 +219,7 @@ class App extends Component {
                     <a href="#"><i class="fa fa-google-plus"></i></a>
                 </div>
             </footer>
-            </div>
+        </div>
 
             <button onClick={this._onImageClick}>
               next
